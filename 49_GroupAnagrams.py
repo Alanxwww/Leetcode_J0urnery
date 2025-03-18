6bd5a7c00f8015a54f2c04b7dbb077e670208f94
Alanxwww <alanwang166@gmail.com>
2025-03-18 10:24:32 -0700

    update

--- 49_GroupAnagrams.py
+++ 49_GroupAnagrams.py
@@ -5,12 +5,12 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = {}                # key:[26*0] -> [str]
 
-        for s in strs:
+        for s in strs:          # loop strings
             l = [0] * 26
-            for c in s:         # loop char
+            for c in s:         # loop character
                 l[ord(c) - ord("a")] += 1
             
-            key = tuple(l)      # convert character to idx(0-25)
+            key = tuple(l)      # A list cannot be a dictionary key
             if key not in res:
                 res[key] = [s]
             else:
