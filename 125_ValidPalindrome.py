bbc68272363e2de78806d9f391d3694542473ac2
Alanxwww <alanwang166@gmail.com>
2025-03-18 17:09:12 -0700

    update

--- 125_ValidPalindrome.py
+++ 125_ValidPalindrome.py
@@ -1,3 +1,12 @@
+'''
+Given a string s, return true if it is a palindrome, otherwise return false.
+A palindrome is a string that reads the same forward and backward. 
+It is also case-insensitive and ignores all non-alphanumeric characters.
+
+Example 1:
+    Input: s = "Was it a car or a cat I saw?"
+    Output: true
+'''
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
